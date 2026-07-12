9b7af2001f502332788718d9771527206f507b64
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:07 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "kusto_cosmosdb_data_connections" {
-  description = "All kusto_cosmosdb_data_connection resources"
-  value       = azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections
-}
 output "kusto_cosmosdb_data_connections_cosmosdb_container_id" {
-  description = "List of cosmosdb_container_id values across all kusto_cosmosdb_data_connections"
-  value       = [for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : v.cosmosdb_container_id]
+  description = "Map of cosmosdb_container_id values across all kusto_cosmosdb_data_connections, keyed the same as var.kusto_cosmosdb_data_connections"
+  value       = { for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : k => v.cosmosdb_container_id }
 }
 output "kusto_cosmosdb_data_connections_kusto_database_id" {
-  description = "List of kusto_database_id values across all kusto_cosmosdb_data_connections"
-  value       = [for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : v.kusto_database_id]
+  description = "Map of kusto_database_id values across all kusto_cosmosdb_data_connections, keyed the same as var.kusto_cosmosdb_data_connections"
+  value       = { for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : k => v.kusto_database_id }
 }
 output "kusto_cosmosdb_data_connections_location" {
-  description = "List of location values across all kusto_cosmosdb_data_connections"
-  value       = [for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : v.location]
+  description = "Map of location values across all kusto_cosmosdb_data_connections, keyed the same as var.kusto_cosmosdb_data_connections"
+  value       = { for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : k => v.location }
 }
 output "kusto_cosmosdb_data_connections_managed_identity_id" {
-  description = "List of managed_identity_id values across all kusto_cosmosdb_data_connections"
-  value       = [for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : v.managed_identity_id]
+  description = "Map of managed_identity_id values across all kusto_cosmosdb_data_connections, keyed the same as var.kusto_cosmosdb_data_connections"
+  value       = { for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : k => v.managed_identity_id }
 }
 output "kusto_cosmosdb_data_connections_mapping_rule_name" {
-  description = "List of mapping_rule_name values across all kusto_cosmosdb_data_connections"
-  value       = [for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : v.mapping_rule_name]
+  description = "Map of mapping_rule_name values across all kusto_cosmosdb_data_connections, keyed the same as var.kusto_cosmosdb_data_connections"
+  value       = { for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : k => v.mapping_rule_name }
 }
 output "kusto_cosmosdb_data_connections_name" {
-  description = "List of name values across all kusto_cosmosdb_data_connections"
-  value       = [for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : v.name]
+  description = "Map of name values across all kusto_cosmosdb_data_connections, keyed the same as var.kusto_cosmosdb_data_connections"
+  value       = { for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : k => v.name }
 }
 output "kusto_cosmosdb_data_connections_retrieval_start_date" {
-  description = "List of retrieval_start_date values across all kusto_cosmosdb_data_connections"
-  value       = [for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : v.retrieval_start_date]
+  description = "Map of retrieval_start_date values across all kusto_cosmosdb_data_connections, keyed the same as var.kusto_cosmosdb_data_connections"
+  value       = { for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : k => v.retrieval_start_date }
 }
 output "kusto_cosmosdb_data_connections_table_name" {
-  description = "List of table_name values across all kusto_cosmosdb_data_connections"
-  value       = [for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : v.table_name]
+  description = "Map of table_name values across all kusto_cosmosdb_data_connections, keyed the same as var.kusto_cosmosdb_data_connections"
+  value       = { for k, v in azurerm_kusto_cosmosdb_data_connection.kusto_cosmosdb_data_connections : k => v.table_name }
 }
 
